d856c3e3ba3f05386c65b4b47ff46046db85aa14
tusar pal <tusarpal97@gmail.com>
2026-07-21 18:54:03 +0530

    change

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { menuItemsData } from "../assets/assets";
 import { NavLink } from "react-router-dom";
-const MenuItems = (setSidebarOpen) => {
+const MenuItems = ({setSidebarOpen}) => {
   return (
     <div className="px-6 text-gray-600 space-y-1 font-medium">
       {menuItemsData.map(({ to, label, Icon }) => (
