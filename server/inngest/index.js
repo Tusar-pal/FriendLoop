0c7efabd32e932848e3db5895b0c385e7db577f5
tusar pal <tusarpal97@gmail.com>
2026-07-25 21:18:12 +0530

    change

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,8 +1,13 @@
 import { Inngest } from "inngest";
 import User from "../models/user.js";
-export const inngest = new Inngest({ id: "friendLoop-app" });
 
-// inngest function to save user data to a database
+export const inngest = new Inngest({
+  id: "friendLoop-app",
+});
+
+// ========================
+// User Created
+// ========================
 
 const syncUserCreation = inngest.createFunction(
   {
@@ -14,14 +19,20 @@ const syncUserCreation = inngest.createFunction(
     ],
   },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      image_url,
+    } = event.data;
 
     let username = email_addresses[0].email_address.split("@")[0];
 
-    const user = await User.findOne({ username });
+    const existingUser = await User.findOne({ username });
 
-    if (user) {
-      username = username + Math.floor(Math.random() * 10000);
+    if (existingUser) {
+      username = `${username}${Math.floor(Math.random() * 10000)}`;
     }
 
     await User.create({
@@ -34,41 +45,58 @@ const syncUserCreation = inngest.createFunction(
   }
 );
 
-// inngest function to update user data in database
+// ========================
+// User Updated
+// ========================
 
-const syncUserUpdation= inngest.createFunction(
-    {
-     id: "update-user-from-clerk",
-    triggers: [{ event: "clerk/user.updated" }],
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [
+      {
+        event: "clerk/user.updated",
+      },
+    ],
   },
-    async ({event})=>{
-        const {id,first_name, last_name, email_address , image_url} = event.data;
+  async ({ event }) => {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      image_url,
+    } = event.data;
 
-        const updateUserData ={
-            email : email_address[0].email_address,
-            full_name: first_name+' '+last_name,
-            profile_picture:image_url
-        }
-        await User.findByIdAndUpdate(id,updateUserData)
-    }
-)
+    await User.findByIdAndUpdate(id, {
+      email: email_addresses[0].email_address,
+      full_name: `${first_name} ${last_name}`,
+      profile_picture: image_url,
+    });
+  }
+);
 
-// inngesr funnction to delete user data in database
+// ========================
+// User Deleted
+// ========================
 
 const syncUserDeletion = inngest.createFunction(
   {
     id: "delete-user-from-clerk",
-    triggers: [{ event: "clerk/user.deleted" }],
+    triggers: [
+      {
+        event: "clerk/user.deleted",
+      },
+    ],
   },
   async ({ event }) => {
     const { id } = event.data;
+
     await User.findByIdAndDelete(id);
   }
 );
 
 export const functions = [
-    syncUserCreation,
-    syncUserUpdation,
-    syncUserDeletion
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion,
 ];
-
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { inngest,functions } from './inngest/index.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import { serve } from "inngest/express";
+
+import connectDB from "./configs/db.js";
+import { inngest, functions } from "./inngest/index.js";
+
 const app = express();
 
 await connectDB();
@@ -11,15 +13,20 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
-
-app.get('/',(req,res)=>{
-    res.send("server is running")
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
-app.use('/api/inngest',serve({client:inngest , functions}))
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
