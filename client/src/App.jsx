7e01285e0eb8f6402eef582a219a47c6c24f087a
tusar pal <tusarpal97@gmail.com>
2026-08-17 21:20:40 +0530

    message send performance change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
@@ -16,11 +16,13 @@ import { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { fetchUser } from "./features/user/userSlice";
 import { fetchConnections } from "./features/connections/connectionsSlice";
+import { addMessage } from "./features/messages/messagesSlice";
 
 const App = () => {
   const { user, isLoaded } = useUser();
   const { getToken } = useAuth();
-
+  const {pathname} = useLocation()
+  const pathnameRef = useRef(pathname)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -51,6 +53,28 @@ const App = () => {
       </div>
     );
   }
+
+  useEffect(()=>{
+    pathnameRef.current = pathname
+  },[pathname])
+
+  useEffect(()=>{
+    if(user){
+      const eventSource = new EventSource(import.meta.env.VITE_BASEURL + '/api/message/'+user.id);
+      eventSource.onmessage= (event)=>{
+        const message = JSON.parse(event.data)
+
+        if(pathnameRef.current === ('/messages/'+message.from_user_id._id)){
+          dispatch(addMessage(message))
+        }else{
+          
+        }
+      }
+      return ()=>{
+        eventSource.close()
+      }
+    }
+  },[user,dispatch])
 
   return (
     <>
