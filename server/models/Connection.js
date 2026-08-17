a6d9daf6393222c7d737a130864fddee474bedac
tusar pal <tusarpal97@gmail.com>
2026-08-17 18:58:49 +0530

    connection model problem

--- server/models/Connection.js
+++ server/models/Connection.js
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
 
 const connectionSchema = new mongoose.Schema({
-    from_user_id: {type: String , ref: 'User' , required: true},
-    to_user_id: {type: String , ref: 'User' , required: true},
-    status: {type: String , enum: ['panding', 'accepted'], default: 'panding'},
+    from_user_id: {
+        type: String,
+        ref: "User",
+        required: true
+    },
 
-},{timestamps: true})
+    to_user_id: {
+        type: String,
+        ref: "User",
+        required: true
+    },
 
-const Connection = mongoose.model('Connection',connectionSchema)
+    status: {
+        type: String,
+        enum: ["pending", "accepted"],
+        default: "pending"
+    }
 
-export default Connection
+}, {
+    timestamps: true
+});
+
+const Connection = mongoose.model("Connection", connectionSchema);
+
+export default Connection;
