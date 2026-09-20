412a4072cec289f7049ca7dd7634de7e401127f7
tusar pal <tusarpal97@gmail.com>
2026-09-21 00:37:31 +0530

    push

--- server/models/user.js
+++ server/models/user.js
@@ -15,6 +15,7 @@ const userSchema = new mongoose.Schema({
 },{timestamps:true , minimize:false})
 
 
-const User = mongoose.model('User',userSchema);
+const User =
+  mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
 import express from "express";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
-
 import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
 
@@ -17,53 +17,27 @@ import commentRouter from "./routes/commentRoutes.js";
 
 const app = express();
 
-// ========================
-// Middlewares
-// ========================
-
 app.use(express.json());
 
 app.use(
   cors({
     origin: [
-      "https://friend-loop-14g5.vercel.app",
       "http://localhost:5173",
+      "https://friend-loop-14g5.vercel.app",
     ],
     credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "PATCH",
-      "DELETE",
-      "OPTIONS",
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
 app.use(clerkMiddleware());
 
-// ========================
-// Database Connection
-// ========================
-
 await connectDB();
-
-// ========================
-// Home Route
-// ========================
 
 app.get("/", (req, res) => {
   res.status(200).send("FriendLoop Server is running");
 });
-
-// ========================
-// Inngest
-// ========================
 
 app.use(
   "/api/inngest",
@@ -73,19 +47,10 @@ app.use(
   })
 );
 
-// ========================
-// Routes
-// ========================
-
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
 app.use("/api/story", storyRouter);
 app.use("/api/message", messageRouter);
-
-
-// ========================
-// Export App for Vercel
-// ========================
 
 export default app;
